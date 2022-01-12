8fa11c10c21b045d32d3b32ff7261de6a29333d3
Betelihem <Betelihem>
2022-01-12 10:22:16 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
-
-    function clickAlert() {
-    alert('I was clicked!');
-    }
-
-    input.addEventListener('click', clickAlert);
 }
-
-addingEventListener();
